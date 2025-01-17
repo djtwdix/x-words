78382571fc6bb3af6ea15bb529356f8823f8283c
djtwdix <djtwd.ix@icloud.com>
2025-01-17 14:49:36 -0400

    Implement Timer and story

---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./Timer.css";
+
+export interface TimerProps {
+  startTime?: number;
+}
+
+export const Timer = ({ startTime = 0 }: TimerProps) => {
+  const [time, setTime] = useState(startTime);
+  const [timeDisplay, setTimeDisplay] = useState("0:00");
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("second");
+      setTime(time + 1);
+    }, 1000);
+
+    function formatTime(seconds: number) {
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const secs = seconds % 60;
+
+      const pad = (num: number) => String(num).padStart(2, "0");
+
+      if (hours) {
+        return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+      } else {
+        return `${pad(minutes)}:${pad(secs)}`;
+      }
+    }
+
+    setTimeDisplay(formatTime(time));
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [time]);
+  return <div className="timer">{timeDisplay}</div>;
+};
---
+++ src/stories/Timer.stories.ts
@@ -0,0 +1,10 @@
+import { Timer } from "../components/Timer/Timer";
+
+const meta = {
+  title: "Components/Timer",
+  component: Timer,
+};
+
+export default meta;
+
+export const Default = {};
