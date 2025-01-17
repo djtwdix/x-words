1b6fa7f855f70caa6cceec77cddc6c42ecb82f87
djtwdix <djtwd.ix@icloud.com>
2025-01-17 14:50:12 -0400

    Create Settings bar and story

---
+++ src/components/SettingsBar/SettingsBar.tsx
@@ -0,0 +1,21 @@
+import { Icon } from "../Icon/Icon";
+import { Timer } from "../Timer/Timer";
+import "./SettingsBar.css";
+
+export interface SettingsBarProps {
+  icons: string[];
+}
+
+export const SettingsBar = ({ icons }: SettingsBarProps) => {
+  console.log("icons: ", icons);
+  return (
+    <div className="settingsBar">
+      <Timer />
+      <div className="icons">
+        {icons.map((icon, index) => {
+          return <Icon key={index} iconName={icon} />;
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ src/stories/SettingsBar.stories.ts
@@ -0,0 +1,16 @@
+import { SettingsBar } from "../components/SettingsBar/SettingsBar";
+
+const meta = {
+  title: "Components/SettingsBar",
+  component: SettingsBar,
+};
+
+export default meta;
+
+const icons = ["list", "edit", "support", "info"];
+
+export const Default = {
+  args: {
+    icons,
+  },
+};
