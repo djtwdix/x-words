746810cf28c23cda8c8b79f35c1e0cded7a494a0
djtwdix <djtwd.ix@icloud.com>
2025-01-22 14:02:13 -0400

    Add context decorator

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,16 @@
 import type { Preview } from "@storybook/react";
+import { PuzzleProvider } from "../src/contexts/PuzzleContext";
+import React from "react";
+import { puzzleData } from "../src/puzzleData";
 
 const preview: Preview = {
+  decorators: (Story) => {
+    return (
+      <PuzzleProvider puzzleData={puzzleData}>
+        <Story />
+      </PuzzleProvider>
+    );
+  },
   parameters: {
     measure: {
       disable: true, // This disables the Measure Addon functionality.
