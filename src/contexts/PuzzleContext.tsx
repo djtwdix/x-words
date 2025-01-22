308c4f6063de347ddc1c4a0dc03cd327fe659f13
djtwdix <djtwd.ix@icloud.com>
2025-01-22 14:01:59 -0400

    Create Puzzle Context

---
+++ src/contexts/PuzzleContext.tsx
@@ -0,0 +1,76 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { PuzzleData } from "../puzzleData";
+
+export const PuzzleContext = createContext<PuzzleContextType | undefined>(
+  undefined
+);
+
+type Orientation = "across" | "down";
+
+export interface PuzzleContextType {
+  puzzleInfo: PuzzleData;
+  setPuzzleInfo: Dispatch<SetStateAction<PuzzleData>>;
+  selectedClueNumber: number | undefined;
+  setSelectedClueNumber: Dispatch<SetStateAction<number | undefined>>;
+  selectedClue: string;
+  orientation: Orientation;
+  changeOrientation: () => void;
+  listView: boolean;
+  setListView: Dispatch<SetStateAction<boolean>>;
+}
+
+export const PuzzleProvider = ({ children, puzzleData }: any) => {
+  const [puzzleInfo, setPuzzleInfo] = useState(puzzleData);
+  const [selectedClue, setSelectedClue] = useState("");
+  const [selectedClueNumber, setSelectedClueNumber] = useState<
+    number | undefined
+  >(0);
+  const [orientation, setOrientation] = useState("across" as Orientation);
+
+  const [listView, setListView] = useState(false);
+
+  useEffect(() => {
+    puzzleData.clues[orientation].forEach((clue: string) => {
+      if (Number(clue[0]) === selectedClueNumber) setSelectedClue(clue);
+    });
+  }, [selectedClueNumber, orientation]);
+
+  //helper function that flips the orientation state
+  const changeOrientation = () => {
+    setOrientation((prev) => (prev === "across" ? "down" : "across"));
+  };
+
+  return (
+    <PuzzleContext.Provider
+      value={{
+        puzzleInfo,
+        setPuzzleInfo,
+        selectedClueNumber,
+        setSelectedClueNumber,
+        selectedClue,
+        orientation,
+        changeOrientation,
+        listView,
+        setListView,
+      }}
+    >
+      {children}
+    </PuzzleContext.Provider>
+  );
+};
+
+export const usePuzzleContext = () => {
+  const puzzleContext = useContext(PuzzleContext);
+
+  if (!puzzleContext)
+    throw new Error("Puzzle context must be used inside of a provider");
+
+  return puzzleContext;
+};
